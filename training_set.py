d55a27e85773696cd732b5bc26c39fc9a30aa508
Jean-Daniel Browne <jeandaniel.browne@gmail.com>
2025-09-30 23:10:39 +0200

    Add  a summary of the extracted  frames. Currently 20X less in_action.

--- training_set.py
+++ training_set.py
@@ -1,6 +1,15 @@
 # training_set offers the write() function to extract frames from videos and read() function to read the extracted frames.
+# training_set also offers a main program to extract frames from videos in a directory.
+# 
+# extract frames from the default directory and save them to the default output directory "training_set/".
+# $ python3 training_set.py 
+#
+# Summarizes the training set in the given directory.
+# $ python3 training_set.py --summary /mnt/chromeos/removable/usbdisk/training_set/
+# Total frames: 22474, In action: 5% (1263), Not in action: 94% (21211).
 
 import argparse
+import collections
 from typing import Callable, Iterator, Tuple, TypeAlias, Optional
 import numpy as np
 from pathlib import Path
@@ -9,12 +18,6 @@ import cv2
 import shutil
 
 Frame: TypeAlias = np.ndarray 
-
-def read(frame_dir: Path) -> Iterator[Tuple[Frame, bool]]:
-    for frame_path in sorted(frame_dir.glob("*/*.jpg")):
-        frame = cv2.imread(str(frame_path))
-        in_action = frame_path.stem.lower().endswith('true')
-        yield frame, in_action
 
 
 def write(video_dir: Path, output_dir: Path, istest: bool) -> None:
@@ -25,7 +28,22 @@ def write(video_dir: Path, output_dir: Path, istest: bool) -> None:
         frames_dir = output_dir / video_path.stem
         frames_dir.mkdir(exist_ok=True)
         frame_path = frames_dir / Path(f'frame_{index:05d}_{str(in_action).lower()}.jpg' )
-        cv2.imwrite(str(frame_path), frame)
+        if in_action:
+          cv2.imwrite(str(frame_path), frame)
+        elif index % 10 == 0:
+          # Save only 10% of the frames not in action.
+          # this should still save 2X more frames not in action than in action.
+          cv2.imwrite(str(frame_path), frame)
+
+
+def read(frame_dir: Path) -> Iterator[Tuple[Path, Frame, bool]]:
+    for frame_path, in_action in _read_paths(frame_dir):
+        yield frame_path, cv2.imread(str(frame_path)), in_action
+
+
+def _read_paths(frame_dir: Path) -> Iterator[Tuple[Path, bool]]:
+    for frame_path in frame_dir.glob("*/*.jpg"):
+        yield frame_path, frame_path.stem.lower().endswith('true')
 
 
 def _extract(video_dir: Path, istest: bool) -> Iterator[Tuple[Path, Frame, bool]]:
@@ -59,7 +77,19 @@ def _frames(video: Path) -> Iterator[Frame]:
           yield frame
     finally:
         cap.release()
-    
+
+
+def _summary(frame_dir: Path) -> str:
+    frame_counts = collections.defaultdict(collections.Counter)
+    for path, in_action in _read_paths(frame_dir):
+        frame_counts[path.parent.name][in_action] += 1
+    total, in_action, not_in_action = 0, 0, 0
+    for counts in frame_counts.values():
+        in_action += counts[True]
+        not_in_action += counts[False]
+        total += counts[False] + counts[True]
+    return f"Total frames: {total}, In action: {int(in_action*100/total)}% ({in_action}), Not in action: {int(100 *not_in_action/total)}% ({not_in_action})."
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Extract the frames annotated with whether the baller is in balling action.")
@@ -69,7 +99,16 @@ if __name__ == '__main__':
                        help="Directory to save the extracted frames. The filename includes whether the baller is in balling action.")
     parser.add_argument("--test", default=False, action='store_true',
                        help="If --test is set, then the frames of a single video is processed.")
+    parser.add_argument("--summary", default=False, action='store_true',
+                       help="When --summary is set, the script displays stats on the produced training set.")
+    parser.add_argument("summary_dir", type=str, default=None,
+                       help="The frames directory for the summary is the first positional arg.")
+
 
     args = parser.parse_args()
-
-    write(Path(args.video_dir), Path(args.output_dir), args.test)
+    if args.summary:
+        if args.summary_dir is None:
+            parser.error("When --summary is set, the frames directory must be provided as the first positional arg.")
+        print(_summary(Path(args.summary_dir)))
+    else:
+        write(Path(args.video_dir), Path(args.output_dir), args.test)
